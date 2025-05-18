c3c74745cdd5acc8a9523a60955ed7a933fc27b1
ChrisTobing <sc22cjt@leeds.ac.uk>
2025-05-18 16:00:51 +0100

    Restart DJANGO API Logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,49 @@ import Game from "./components/Game"; // Import the new Game component
 import HeroPage from "./components/HeroPage";
 import { useEffect, useState } from "react";
 import { socket } from "./socket"; // Import the socket instance
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [gameObj, setgameObj] = useState({
+    state: {
+      playersReady: 0,
+      currentRound: 0,
+      numPlayers: 0,
+      roundFinished: false,
+    },
+    players: {},
+    playerNumberFromId: {},
+    roomName: null,
+  });
 
-    const [gameObj, setgameObj] = useState({
-      state: {  
+  const [playerNumber, setPlayerNumber] = useState(null);
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    socket.on("game-update", (gameObj) => {
+      setgameObj(gameObj);
+      console.log("Game Update Received: ", gameObj);
+    });
+    socket.on("player-number", (playerNumber) => {
+      setPlayerNumber(playerNumber);
+      console.log("received player number: ", playerNumber);
+    });
+    socket.on("unknownCode", () => {
+      setErrorMessage("Error: Unknown Room Code!");
+    });
+    socket.on("tooManyPlayers", () => {
+      setErrorMessage("Error: Room is full!");
+    });
+    socket.on("game-end", (gameEnd) => {
+      setgameObj(gameEnd.game);
+      console.log("Game Ended: ", gameEnd);
+    });
+  }, []);
+
+  function handleMainMenu() {
+    setgameObj({
+      state: {
         playersReady: 0,
         currentRound: 0,
         numPlayers: 0,
@@ -17,56 +55,31 @@ function App() {
       playerNumberFromId: {},
       roomName: null,
     });
-
-    const [playerNumber, setPlayerNumber] = useState(null)
-
-    const [errorMessage, setErrorMessage] = useState(null);
-
-    useEffect(() => {
-      socket.on("game-update", (gameObj) => {
-        setgameObj(gameObj);
-        console.log("Game Update Received: ", gameObj);
-      });
-      socket.on("player-number",(playerNumber)=>{
-        setPlayerNumber(playerNumber)
-        console.log('received player number: ', playerNumber)
-      })
-      socket.on("unknownCode", () => {
-        setErrorMessage("Error: Unknown Room Code!");
-      });
-      socket.on("tooManyPlayers", () => {
-        setErrorMessage("Error: Room is full!");
-      });
-      socket.on("game-end", (gameEnd) => {
-        setgameObj(gameEnd.game);
-        console.log("Game Ended: ", gameEnd);
-      });
-    }, []);
-
-    function handleMainMenu() {
-      setgameObj({
-        state: {  
-          playersReady: 0,
-          currentRound: 0,
-          numPlayers: 0,
-          roundFinished: false,
-        },
-        players: {},
-        playerNumberFromId: {},
-        roomName: null,
-      });
-      setPlayerNumber(null);
-      setErrorMessage(null);
-    }
+    setPlayerNumber(null);
+    setErrorMessage(null);
+  }
 
   return (
-    <div className="App">
-      {gameObj.roomName ? 
-        <Game gameObj={gameObj} playerNumber={playerNumber} handleMainMenu={handleMainMenu} /> // Pass the game state to the Game component
-       : 
-        <HeroPage errorMessage={errorMessage} />
-      }
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HeroPage errorMessage={errorMessage} />} />
+          <Route
+            path="/game:roomId"
+            element={<HeroPage errorMessage={errorMessage} />}
+          />
+          {gameObj.roomName ? (
+            <Game
+              gameObj={gameObj}
+              playerNumber={playerNumber}
+              handleMainMenu={handleMainMenu}
+            /> // Pass the game state to the Game component
+          ) : (
+            <HeroPage errorMessage={errorMessage} />
+          )}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
